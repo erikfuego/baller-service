bfe619dc7535ae327d0b30d2ed349087f204b830
Erik Rosales <rerick786@gmail.com>
2023-06-06 22:30:11 -0400

    Added Dockerfile

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 	router.GET("/hello-world", helloWorld)
 
-	router.Run("localhost:8080")
+	router.Run(":8080")
 }
 
 func helloWorld(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, gin.H{ "hello": "world!" })
+	c.IndentedJSON(http.StatusOK, "Hello, World!")
 }
